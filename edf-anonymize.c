38e01e11f773339c9beb1c2a0a988fe983baf5bf
alesparza <aesparza@seas.upenn.edu>
2023-01-10 21:26:13 -0500

    style: remove extra spacings

--- edf-anonymize.c
+++ edf-anonymize.c
@@ -20,17 +20,17 @@ void miniHexDump(char* filename) {
   printf("Reviewing file %s\n", filename);
 
   FILE* file = fopen(filename, "rb");
-
   if (file == NULL) {
     printf("Error opening file, is the filename correct? (tried %s)\n", filename);
     exit(1);
   }
+
   printf("Printing header for %s\n", filename);
   for (int i = 0; i < HEADER_LENGTH; i++) {
     printBinaryRow(i, file);
   }
-
   printf("Done!\n");
+
   return;
 }
 
@@ -44,13 +44,11 @@ char* setOutputFilename(char* inputFileName) {
   }
   strcpy(extensionIndex, DEID_EDF_EXTENSION);
 
-
   printf("Set output filename to %s\n", outputFileName);
   return outputFileName;
 }
 
 int main(int argc, char **argv) {
-
   if (argc < 2) {
     printf("Invalid number of arguments, need to provide the input file!\n");
     exit(1);
@@ -59,7 +57,6 @@ int main(int argc, char **argv) {
   char* inputFileName = argv[1];
   char* outputFileName = setOutputFilename(inputFileName);
   miniHexDump(argv[1]);
-
 
   return 0;
 }
