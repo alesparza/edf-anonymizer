899fc7f632fa3047a2679f2d602b1940ecc8f965
alesparza <aesparza@seas.upenn.edu>
2023-01-11 22:19:09 -0500

    fix: force message to print before running

--- edf-anonymizer.c
+++ edf-anonymizer.c
@@ -41,6 +41,7 @@ int main(int argc, char **argv) {
   }
   printf("You entered: %s\n", newData);
   printf("Anonymizing file (this can take a bit for large files)");
+  fflush(stdout);
 
   FILE* input = fopen(inputFileName, "rb");
   FILE* output = fopen(outputFileName, "wb");
