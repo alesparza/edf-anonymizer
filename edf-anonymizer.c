890d11dc6300695506a88d67af1e6bebf83000cb
alesparza <aesparza@seas.upenn.edu>
2023-01-14 19:23:07 -0500

    docs: update section comments

--- edf-anonymizer.c
+++ edf-anonymizer.c
@@ -30,6 +30,7 @@ char* getInputName() {
 }
 
 char* setOutputFilename(char* inputFileName) {
+  // copy the current filename to the new buffer, then add the appropriate extension
   char* outputFileName = malloc(sizeof(char) * (strlen(inputFileName) + strlen(DEID_FILE_SUFFIX) + 1));
   strncpy(outputFileName, inputFileName, strlen(inputFileName));
   char* extensionIndex = strstr(outputFileName, EDF_EXTENSION);
@@ -38,23 +39,23 @@ char* setOutputFilename(char* inputFileName) {
     exit(1);
   }
   strncpy(extensionIndex, DEID_EDF_EXTENSION, strlen(DEID_EDF_EXTENSION));  // copies new extension to filename
-
   printf("Set output filename to %s\n", outputFileName);
   return outputFileName;
 }
 
 int main(int argc, char **argv) {
-  // handle arguments
   if (argc < MINIMUM_ARGUMENTS) {
     printHelp();
     exit(1);
   }
 
+  // flags for various arguments
   char* inputFileName = NULL;
   int isSimple = 0;
   int isDetail = 0;
   int isReview = 0;
 
+  // check all the arguments, set flags, report errors (or print the help message)
   for (int i = 0; i < argc; i++) {
     //TODO: switch to switch block
     if (strcmp("-i", argv[i]) == 0) {
@@ -93,12 +94,14 @@ int main(int argc, char **argv) {
     }
   }
 
+  // these are mutually exclusive
   if (isSimple && isDetail) {
     printf("Cannot request Simple and Detail mode\n");
     printHelp();
     exit(1);
   }
 
+  // ensure we have some kind of filename
   if (inputFileName == NULL) {
     inputFileName = getInputName();
   }
@@ -113,19 +116,18 @@ int main(int argc, char **argv) {
   char* outputFileName = setOutputFilename(inputFileName);
   char* newData = calloc(LOCAL_PATIENT_IDENFITICATION_LENGTH + 1, sizeof(char));
 
-  // Detail Mode; ask for each field
+  // Detail Mode; ask for each field individually
   if (isDetail) {
     char* patientID = calloc(BUFFER_SIZE, sizeof(char));
     char* patientSex = calloc(BUFFER_SIZE, sizeof(char));
     char* patientDOB = calloc(BUFFER_SIZE, sizeof(char));
     char* patientName = calloc(BUFFER_SIZE, sizeof(char));
     char* tempBuffer = calloc(BUFFER_SIZE * 4, sizeof(char));
-    //TODO: actually implement this
     printf("Please enter a response for the following four questions.  Press enter if unknown or not used\n");
+
+    // for each prompt, check if user pressed enter, if so, write "X ", otherwise just remove the last \n
     printf("Please enter the patient ID: ");
     fgets(patientID, BUFFER_SIZE, stdin);
-
-    // check if user pressed enter, if so, write "X ", otherwise just remove the last \n
     if (*patientID == '\n') {
       *patientID = 'X';
       *(patientID + 1) = ' ';
@@ -160,13 +162,13 @@ int main(int argc, char **argv) {
       *(patientName + strlen(patientName) - 1) = '\0';
     }
 
+    // put all the parts together, then ensure it is the right length and has spaces for empty bytes
     strcat(tempBuffer, patientID);
     strcat(tempBuffer, patientSex);
     strcat(tempBuffer, patientDOB);
     strcat(tempBuffer, patientName);
-    int len = strlen(tempBuffer);
 
-    // ensure the temp buffer has the appropriate length
+    int len = strlen(tempBuffer); // TODO: print message if length is too long
     for (int i = len; i < LOCAL_PATIENT_IDENFITICATION_LENGTH; i++) {
       *(tempBuffer + i) = ' ';
     }
@@ -176,7 +178,7 @@ int main(int argc, char **argv) {
   } else {
     // Simple Mode; single prompt for the entire field
     printf("Please enter replacement data for Local Patient Identification (80 character max): ");
-    fgets(newData, LOCAL_PATIENT_IDENFITICATION_LENGTH, stdin);
+    fgets(newData, LOCAL_PATIENT_IDENFITICATION_LENGTH, stdin); //TODO: print error if too long
     *(newData + strlen(newData) - 1) = '\0'; // removing the \n
     for (int i = 0; i < LOCAL_PATIENT_IDENFITICATION_LENGTH; i++) {
       if (*(newData + i) == '\0') {
@@ -190,12 +192,12 @@ int main(int argc, char **argv) {
   FILE* input = fopen(inputFileName, "rb");
   FILE* output = fopen(outputFileName, "wb");
 
-  // copy the version
+  // copy the version; this is the only part before the patient info section
   int version[VERSION_LENGTH];
   fread(version, VERSION_LENGTH, sizeof(char), input);
   fwrite(version, VERSION_LENGTH, sizeof(char), output);
 
-  // write the local data
+  // write the local patient info data
   int buffer[BUFFER_SIZE];
   memset(buffer, '\0', sizeof(buffer));
   fread(buffer, LOCAL_PATIENT_IDENFITICATION_LENGTH, sizeof(char), input);
@@ -208,13 +210,12 @@ int main(int argc, char **argv) {
     fwrite(buffer, sizeof(buffer), sizeof(char), output);
     memset(buffer, '\0', sizeof(buffer));
   }
+  printf("Done writing the output file %s\n", outputFileName);
 
-
+  // cleanup
+  miniHexDump(outputFileName, HEADER_LENGTH);
   fclose(input);
   fclose(output);
-  printf("Done writing the output file %s\n", outputFileName);
-  miniHexDump(outputFileName, HEADER_LENGTH);
-
   free(outputFileName);
 
   return 0;
